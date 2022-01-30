4f583f6cd8b5dcd549ade23392c0c9beb831793b
Samuel O. Asare <asare11samuel@gmail.com>
2022-01-30 18:50:18 +0000

    ref(app.test.js): DRY (Don't Repeat Yourself) refactor

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
-import Enzyme, {shallow} from 'enzyme'
+/* eslint-disable testing-library/await-async-query */
+import Enzyme, {shallow, ShallowWrapper} from 'enzyme'
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
 import App from './App'
 
@@ -6,24 +7,34 @@ Enzyme.configure({
   adapter: new EnzymeAdapter()
 })
 
+/**
+ * Function to create default shallow wrapper for the App component
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+
+const setup = ()=>shallow(<App />)
+const findByTestAttribute = (wrapper, val)=>wrapper.find(`[data-test='${val}']`)
+
 test('renders without error', ()=>{
-    const wrapper = shallow(<App />)
-    const appComponent = wrapper.find("[data-test='component-app']")
+    const wrapper = setup()
+    // eslint-disable-next-line testing-library/await-async-query
+    const appComponent = findByTestAttribute(wrapper, 'component-app')
     expect(appComponent.length).toBe(1)
 })
 
 test('renders increment button', ()=>{
 
-  const wrapper = shallow(<App />)
-  const incrementButton = wrapper.find("[data-test='increment-button']")
+  const wrapper = setup()
+  const incrementButton = findByTestAttribute(wrapper, 'increment-button')
   expect(incrementButton.length).toBe(1)
   
 })
 
 test('renders counter display',()=>{
 
-  const wrapper = shallow(<App />)
-  const counterDisplay = wrapper.find("[data-test='counter-display']")
+  const wrapper = setup()
+  const counterDisplay = findByTestAttribute(wrapper, 'counter-display')
   expect(counterDisplay.length).toBe(1)
   
 })
